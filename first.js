6dd0e1dcf04131a455fdccdf5f5d5ceb000b83ba
Abhishek Rawat <abr07k@gmail.com>
2025-01-22 15:14:42 +0530

    first

---
+++ first.js
@@ -0,0 +1,35 @@
+// name = "Abhishek";
+// var a = 12;
+// a = ab;
+// console.log(a);  
+// {
+// let a ;
+// console.log(a);
+// }
+
+// a = 16;
+// console.log(a);
+
+const student = {
+    fullname : 'lala',
+    age :12,
+    ispass : true
+};
+// console.log(student.fullname);
+student.fullname = 'rana';
+// console.log(student.fullname);
+
+
+const product = {
+    name : "abhishek",
+    rating : 5,
+    offer : 12,
+    price :321
+};
+
+
+
+str = "khadu";
+for(let i of str){
+    console.log(i);
+}
