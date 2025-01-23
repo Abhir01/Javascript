b972233744beb5a121fe3fd76d82d7cf9dce7b30
Abhishek Rawat <abr07k@gmail.com>
2025-01-24 00:03:42 +0530

    23 jan

---
+++ first.js
@@ -0,0 +1,71 @@
+// name = "Abhishek";
+// var a = 12;
+// a = ab;
+// console.log(a);  
+// {
+// let a ;
+// console.log(a);
+// }
+
+// a = 16;
+// console.log(a);
+
+// const student = {
+//     fullname : 'lala',
+//     age :12,
+//     ispass : true
+// };
+// console.log(student.fullname);
+// student.fullname = 'rana';
+// console.log(student.fullname);
+
+
+const product = {
+    name : "abhishek",
+    rating : 5,
+    offer : 12,
+    price :321
+};
+
+
+//for of loop
+
+// str = "khadu";
+// for(let i of str){
+//     console.log(i);
+// }
+
+//for in loop
+
+// for(let i in product){
+//     console.log(i , product[i]);
+// }
+
+// let a = prompt("enter a number: ");
+// if(a % 5 == 0){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// console.log(`the price of ${product.name} is ${product.price}`);
+// console.log("the price of " , product.name , "is", product.price``
+// );
+
+
+// let arr = ["abhishek" , "kaka" , "kalu" , "peelu"];
+// for(let i of arr){
+//     console.log(i);
+// }
+
+// for(let i = 0; i<arr.length ; i++){
+//     console.log(arr[i]);
+// }
+
+// arr.pop();
+// console.log(arr);
+
+// console.log(arr.toString());
+
+// console.log(arr);
---
+++ second.js
@@ -0,0 +1,131 @@
+// function fun(){
+//     console.log("whats up");
+//     console.log("hello");
+// }
+// fun();
+
+// function fun(param){
+//     console.log(param);
+// }
+// fun("argument");
+
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// sum(1,2);
+
+//rather than printing we are returning
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(1,2));
+
+//arrow functions 
+// const sum = (a,b)=>{
+//     console.log(a+b);
+// }
+// sum(1,2);
+
+//count no of vowels
+// function vow(param){
+//     let cnt = 0;
+//     for(let i of param){
+//         if(i === 'a' || i === 'i' || i === 'e' || i === 'o' || i ==='u'){
+//             cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+// console.log(vow("hello"));
+
+// count vowels using arrow function
+// const cntvow = (param) => {
+//     let cnt = 0;
+//     for(let i of param){
+//         if(i === 'a' || i === 'i' || i === 'e' || i === 'o' || i ==='u'){
+//             cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+// console.log(cntvow("helloo"));
+
+//call back function -> fucntion which is passed as an argument to another fucntion
+
+//foreach loop in arrays
+let arr = [1,2,3,4,5];
+
+// arr.forEach(function printVal(val){
+//     console.log(val);
+// })
+
+// arr.forEach(function twice(val){
+//     console.log(val*2);
+// })
+
+// generally as an arrow fucntion we pass fucntion in forEach
+// arr.forEach((val) => {
+//     console.log(val*3);
+// })
+
+//optional parameters in foreach ka callback
+// arr.forEach((val , idx , arr) =>{
+//     console.log(val , idx , arr) ;
+// })
+
+// hof -> a function that uses another function as a parameter 
+//        and can return the function as a value 
+
+
+// arr.forEach((val) => {
+//     console.log(val*val);
+// })
+// this can be written as:
+
+// let square = (val) =>{
+//     console.log(val*val);
+// }
+// arr.forEach(square);
+
+//array methods
+
+//map method is mostly similar like foreach just it returns new arr
+
+// map
+// arr.map((val)=>{
+//     console.log(val);
+// })
+
+// let newArr = arr.map( (val) => {
+//     return val;
+// })
+// console.log(newArr);
+
+// filter 
+// let fil = arr.filter((val) => {
+//     return val%2===0;
+// })
+// console.log(fil);
+
+// reduce
+
+//sum of array
+// const op  = arr.reduce((acc,val) => {
+//     return acc+val;
+// },0);
+// console.log(op);
+
+//max of an array
+const max = arr.reduce((acc,val) => {
+    if(val > acc){
+        acc = val;
+    }return acc;
+},0);
+console.log(max);
+
+
+
+
+
+
